a2117223b799f4e95ddd4b7635c0c5d3b0d5f8b8
nukoww <johndenielescuro@gmail.com>
2024-05-15 22:14:06 +0800

    add missing {

--- escuro_johndeniel/script.js
+++ escuro_johndeniel/script.js
@@ -1,11 +1,11 @@
 let comments = [];
 
 function validateInput() {
-  let inputField = document.querySelector("#comment");
-  let nameField = document.querySelector("#name");
-  let submitBtn = document.querySelector("#submit-btn");
+  let inputField = document.getElementById("comment");
+  let nameField = document.getElementById("name");
+  let submitBtn = document.getElementById("submit-btn");
 
-  if (inputField.value.trim().length && nameField.value.trim().length) {
+  if (!inputField.value.trim().length && !nameField.value.trim().length) {
     submitBtn.disabled = false;
   } else {
     submitBtn.disabled = true;
@@ -13,9 +13,9 @@ function validateInput() {
 }
 
 function addComment() {
-  let nameField = document.querySelector("#name");
-  let commentField = document.querySelector("#comment");
-  let commentList = document.querySelector("#comment-list");
+  let nameField = document.getElementById("name");
+  let commentField = document.getElementById("comment");
+  let commentList = document.getElementById("comment-list");
 
   let comment = {
     name: nameField.value.trim(),
@@ -28,11 +28,11 @@ function addComment() {
 
   nameField.value = "";
   commentField.value = "";
-  document.querySelector("#submit-btn").disabled = true;
+  document.getElementById("submit-btn").disabled = true;
 }
 
 function renderComments() {
-  let commentList = document.querySelector("#comment-list");
+  let commentList = document.getElementById("comment-list");
   commentList.innerHTML = "";
 
   comments.forEach((comment) => {
