da45bde77e9da3a77db11f17a09ae379096e2c0b
Trinidad, Kian Lee B. <trinidadkianleeb@gmail.com>
2024-05-15 09:59:29 +0800

    add exercise 11 This is my files from exercise 11 and our index.

---
+++ trinidad_kianlee/script.js
@@ -0,0 +1,11 @@
+function checkComment() {
+  let userName = document.getElementById("user_name");
+  let userComment = document.getElementById("user_comment");
+  let commentButton = document.getElementById("comment_button");
+
+  if (userName.value.length > 0 && userComment.value.length > 0) {
+    commentButton.disabled = false;
+  } else {
+    commentButton.disabled = true;
+  }
+}
